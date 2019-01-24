1b4f26db9f57607ba2f167257f15d50412e0d272
Blake Swanson <225774@c1mww5vcj1wk.isd720.com>
2019-01-24 11:10:52 -0600

    Update team11.py
    Chnaged program to start continuoulsy betraty after being betrayed once.

--- team11.py
+++ team11.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Blake Swanson Team' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'Colludes until opponent has betrayed 2 times or more, then betrays'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -33,7 +33,7 @@ def move(my_history, their_history, my_score, their_score):
         for i in their_history:
             if i == 'b':
                 total += 1
-        if total >2:
+        if total >1:
             return 'b'
         else:
             return 'c'
